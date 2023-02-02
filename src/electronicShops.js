ec3c73821193b5bca0c5e03b6f00070b420715aa
frank renato <frankfigao@gmail.com>
2023-02-02 08:31:40 -0600

    Adding electronic shops problem

---
+++ src/electronicShops.js
@@ -0,0 +1,16 @@
+const electronicShops = (keyboards, drives, b)=>{
+  const choices= [];
+  for(let i=0; i< keyboards.length; i++){
+    
+    for(let j= 0; j< drives.length; j++){
+       let possibleChoice = keyboards[i]+ drives[j];
+       if(possibleChoice <= b){
+        choices.push(possibleChoice)
+       }
+    }
+  }
+  const result  = choices.length >0 ? Math.max(...choices):-1   
+ return result;
+}
+
+electronicShops([40, 50,60], [5,8,12], 60);
