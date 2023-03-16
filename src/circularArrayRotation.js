d4f1f6c3240ab72cbebcdcf683bd2abfe7f5115f
frank renato <frankfigao@gmail.com>
2023-03-15 20:04:12 -0600

    Adding permutationEquation

--- src/circularArrayRotation.js
+++ src/circularArrayRotation.js
@@ -1,9 +1,7 @@
 const circularArrayRotation =(a, k, queries) =>{
   const n = a.length;
-  k = k % n;
-
+  k = k % n;  
   let rotated = [...a.slice(n - k), ...a.slice(0, n - k)];
-  return  queries.map(q=> rotated[q])
-  
+  return  queries.map(q=> rotated[q])  
 }
 console.log(circularArrayRotation([3, 4,5], 2, [1,2]));
